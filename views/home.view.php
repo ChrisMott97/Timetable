1eb36b9e5f4b8e4e147605d39ef2169dc1742ee9
Chris Mott <christopheramott@sky.com>
2016-10-10 07:44:49 +0100

    Added map

--- views/home.view.php
+++ views/home.view.php
@@ -6,11 +6,14 @@
 </head>
 
 <body>
-       <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1><?= "Welcome, $user->firstname" ?></h1>
-        <p>Below is a basic map of the school.</p>
-      </div>
+    <div class="container">
+        <div class="jumbotron">
+            <h1><?= "Welcome, $user->firstname" ?></h1>
+            <p>Below is a basic map of the school.</p>
+        </div>
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <img class="img-responsive" src="../public/images/denefield.png">
+            </div>
+        </div>
     
--- views/index.view.php
+++ views/index.view.php
@@ -7,13 +7,16 @@
 </head>
 
 <body>     
-       <div class="container">
-
-      <!-- Main component for a primary marketing message or call to action -->
-      <div class="jumbotron">
-        <h1><?= $username ?></h1>
-        <p>Below is a basic map of the school.</p>
-        <p>To use the built in planner and get the quickest routes to lessons, please login or register!</p>
-        
-      </div>
+    <div class="container">
+        <div class="jumbotron">
+            <h1><?= $username ?></h1>
+            <p>Below is a basic map of the school.</p>
+            <p>To use the built in planner and get the quickest routes to lessons, please login or register!</p>
+        </div>
+        <div class="panel panel-primary">
+            <div class="panel-body">
+                <img class="img-responsive" src="../public/images/denefield.png">
+            </div>
+        </div>
+           
 

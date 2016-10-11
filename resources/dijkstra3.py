81ad9859ed9bd36911dd51ae3c29a64aa3861417
Chris Mott <christopheramott@sky.com>
2016-10-11 13:00:16 +0100

    Continued to transfer dijsktra from flowchart to python

--- resources/dijkstra3.py
+++ resources/dijkstra3.py
@@ -25,14 +25,26 @@ class Graph(object):
     A graph instantiates and groups Nodes.
     Attributes:
         name: a string representing the name of the graph
-        nodes: a dictionary of node identifiers associated with their node object      
+        nodes: a dictionary of node instances
+        startNode: a string representing the start point
+        endPoint: a string representing the end point
+        currentNode: a node instance reprenting the current node being used
+        unvisited: a list of node identifiers as strings representing unvisited nodes
+        finalLength: an integer representing the final length of the shortest path
+        finalRoute: a list of node identifiers as strings representing the final shortest path route
     '''
 
     name = "the graph"
+    
     nodes = {}
     startNode = str()
     endNode = str()
     currentNode = object()
+
+    unvisited = list()
+    
+    finalLength = int()
+    finalRoute = list()
     
     def __init__(self, graph_dict):
         for node, connections in graph_dict.items():
@@ -40,22 +52,40 @@ class Graph(object):
             for connector, distance in connections.items():
                 connectors[connector] = str(distance)
             self.nodes[node] = Node(node, False, None, None, connectors)
-
+        
     def listNodes(self):
         nodelist = []
         for node in self.nodes:
             nodelist.append(node)
         return nodelist
     
-    #def findShortest(self, start, end):
-    #    for node in self.nodes:
-            
+    def findShortest(self, start_node, end_node):
+        self.startNode = start_node
+        self.endNode = end_node
+        self.currentNode = self.nodes[self.startNode]
+        self.unvisited.append(self.currentNode.identifier)
+
+        for connector, length in self.currentNode.connectors.items():
+            connectorObject = self.nodes[connector]
+            if((connectorObject.shortest == None) or (length < connectorObject.shortest)):
+                if(self.currentNode.previous):
+                    connectorObject.shortest = self.nodes[self.currentNode.previous].shortest + length
+                else:
+                    connectorObject.shortest = length
+                connectorObject.previous = self.currentNode.identifier
+                if(connectorObject.visited != True):
+                    self.unvisited.append(connectorObject.identifier)
+        self.unvisited.remove(self.currentNode.identifier)
+        self.currentNode.visited = True
 
 newgraph = Graph(graph)
 print("This graph contains nodes "+(str(newgraph.listNodes())))
 print("This graph includes vertex "+newgraph.nodes['A'].identifier)
-print("This node is connected to "+(str(newgraph.nodes['A'].connectors).rstrip('}').lstrip('{')))
+print("This node is connected to "+(str(newgraph.nodes['A'].connectors)))
 if(newgraph.nodes['A'].visited):
     print("and it has been visited")
 else:
     print("and it has not been visited")
+newgraph.findShortest('A','B')
+print(newgraph.unvisited)
+

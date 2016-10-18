4f29e20ef6171559acebe37e247d26075a937e07
Chris Mott <christopheramott@sky.com>
2016-10-18 10:34:58 +0100

    Updated home page layout view with new notifications section and today's schedule section

--- views/index.view.php
+++ views/index.view.php
@@ -4,10 +4,10 @@
         <p>Below is a basic map of the school.</p>
         <p>To use the built in planner and get the quickest routes to lessons, please login or register!</p>
     </div>
+</div>
     <div class="panel panel-default">
         <div class="panel-body">
             <img class="img-responsive" src="../public/images/denefield.png">
         </div>
     </div>
-</div>
 <?php echo $_SERVER['REQUEST_URI']; ?>

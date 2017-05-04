8c20438fa06a970984223974da1e8cc70948844f
Chris <christopheramott@sky.com>
2017-05-04 17:52:34 +0100

    Added time to home page

--- views/footer.view.php
+++ views/footer.view.php
@@ -26,5 +26,6 @@
 		<script src="/public/js/timetable.js"></script>
 		<script src="/public/js/periodtimes.js"></script>
 		<script src="/public/js/timetable_update.js"></script>
+		<script src="/public/js/clock.js"></script>
 	</body>
 </html>
--- views/home.view.php
+++ views/home.view.php
@@ -1,5 +1,8 @@
 <div class="container">
 	<div class="card-panel">
+		<h3 id="time"></h3>
+	</div>
+	<div class="card-panel">
 		<h1><?= "Welcome, $user->firstname" ?></h1>
 		<p>Below is a basic map of the school.</p>
 	</div>
